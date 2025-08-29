bf9c1b84ab133bb2b87b00c92dceb48cb39e29a3
W W <2908869213@qq.com>
2025-08-29 17:24:41 +0800

    version 0.0.3: test third party Eigen

--- src/process/process.cpp
+++ src/process/process.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 #include "process.h"
+#include <Eigen/Dense>
+using Eigen::MatrixXd;
 using std::cout, std::endl;
 
 void Process::planProcess()
 {
   cout << "This is planing process" << endl;
   my_map.mapInfo();
+
+  MatrixXd m(2, 2);
+  m(0, 0) = 2;
+  m(1, 0) = 3;
+  m(0, 1) = 4;
+  m(1, 1) = 5;
+  cout << m << endl;
+
   cout << "planning success!" << endl;
 }
